336958b06a240ac0eafe164dfa4e382d023b31c0
jayb2302 <jonbja01@easv365.dk>
2025-02-22 14:44:05 +0100

    🔗 Connected project to new repository

---
+++ src/modules/useProducts.ts
@@ -0,0 +1,34 @@
+import { ref } from "vue";
+import type { Product } from "../interfaces/interfaces";
+
+
+export const useProducts = () => {
+  const error = ref<string | null>(null);
+  const loading = ref<boolean>(false);
+  const products = ref<Product[]>([]);
+
+  const fetchProducts = async (): Promise<void> => {
+    loading.value = true;
+    try {
+      const response = await fetch(
+        "http://localhost:4000/api/products"
+      );
+      if (!response.ok) {
+        throw new Error("No data available");
+      }
+      const data: Product[] = await response.json();
+      products.value = data;
+      console.log("Products fetched:", products.value);
+    } catch (err) {
+      error.value = (err as Error).message;
+    } finally {
+      loading.value = false;
+    }
+  };
+  return {
+    error,
+    loading,
+    products,
+    fetchProducts,
+  };
+};
